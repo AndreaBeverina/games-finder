c704194535661e62b51e99d5d52d207fd84aac00
Andrea Beverina <andrea.beverina@gmail.com>
2023-04-08 13:48:28 +0200

    test api post

--- src/chiamate/chiamate.ts
+++ src/chiamate/chiamate.ts
@@ -1,25 +1,33 @@
+import axios from "axios";
 import fakeDB from "../db/fakeDB.json"
 import { GameType } from "../model/GameType";
+
+
+const apiUrl = 'https://dk8far7kja.execute-api.us-east-1.amazonaws.com/prod-fase2';
+const data = { "title": "min" };
 
 export const postVideogame = async (titolo: string) => {
     //TODO: fare questo metodo 
     await timeout(1000);
-    const gameList : GameType[] = fakeDB.games;
+    const gameList: GameType[] = fakeDB.games;
+
+    const gameList2 = await axios.post(apiUrl, data);
+    console.log(gameList2)
     return gameList;
 }
 
-export const getGame = async (id : number) => {
+export const getGame = async (id: number) => {
     //await timeout(1000);
-    const gameList : GameType[] = fakeDB.games;
-    let game : GameType = gameList[id];
+    const gameList: GameType[] = fakeDB.games;
+    let game: GameType = gameList[id];
 
     return game;
 }
 
-export const getSuggestions = async (game : GameType) => {
+export const getSuggestions = async (game: GameType) => {
     //TODO: fare il metodo per bene
     await timeout(1000);
-    const gameList : GameType[] = fakeDB.games;
+    const gameList: GameType[] = fakeDB.games;
     return gameList;
 }
 
@@ -27,7 +35,7 @@ export const getSuggestions = async (game : GameType) => {
 
 ///////////////////////
 
-function timeout(ms : any) {
+function timeout(ms: any) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
